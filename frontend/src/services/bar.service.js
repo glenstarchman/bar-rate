ca40106238f6debab8a6843b1fb51c1870811390
Glen Starchman <glen@starchman.com>
2019-06-14 08:00:35 -0700

    snapshot

--- frontend/src/services/bar.service.js
+++ frontend/src/services/bar.service.js
@@ -3,6 +3,6 @@ import {callApi} from '../common/fetcher.js';
 
 
 export const getBar = (id) => {
-  let url = 'bar/' + id.toString();
+  let url = `bar/${id.toString()}/`;
   return callApi(url, 'GET', null);
 }
--- frontend/src/services/bartender.service.js
+++ frontend/src/services/bartender.service.js
@@ -3,6 +3,6 @@ import {callApi} from '../common/fetcher.js';
 
 
 export const getBartender = (id) => {
-  let url = 'bartender/' + id.toString();
+  let url = `bartender/${id.toString()}/`;
   return callApi(url, 'GET', null);
 }
